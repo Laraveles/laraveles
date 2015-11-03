d580b7a9f588b5d061f930e1476897f03f2f9275
Israel Ortuño <ai.ortuno@gmail.com>
2015-11-03 17:13:34 +0100

    Initial commit

---
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Laraveles\Http\Middleware;
+
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
+
+class VerifyCsrfToken extends BaseVerifier
+{
+    /**
+     * The URIs that should be excluded from CSRF verification.
+     *
+     * @var array
+     */
+    protected $except = [
+        //
+    ];
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+## Laraveles
+
+Proyecto del sitio Laraveles.com. Actualmente en fase de desarrollo.
+
+### Licencia
+
+Este proyecto es de código abierto y se establece bajo la licencia [MIT license](http://opensource.org/licenses/MIT)
