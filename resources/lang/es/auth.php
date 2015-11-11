ac79f8985f7de7169c39ace523c779be86167b2e
Israel Ortuño <ai.ortuno@gmail.com>
2015-11-11 15:43:09 +0100

    Added inactive message

--- resources/lang/es/auth.php
+++ resources/lang/es/auth.php
@@ -15,5 +15,6 @@ return [
 
     'failed' => 'Estas credenciales no coinciden con nuestros registros.',
     'throttle' => 'Demasiados intentos de inicio de sesión. Vuelva a intentarlo en :seconds segundos.',
+    'inactive' => 'El e-mail de este usuario todavía no ha sido confirmado. Compruebe su bandeja de entrada o solicite un nuevo enlace de confirmación.'
 
 ];
