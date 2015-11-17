3c9cd8c532edccb11fca6f5800b28518e9aeb15c
Israel Ortuño <ai.ortuno@gmail.com>
2015-11-17 12:31:02 +0100

    Added ide helper

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace Laraveles\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Contracts\Bus\Dispatcher;
+use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -16,8 +17,20 @@ class AppServiceProvider extends ServiceProvider
     {
         // Registering how command bus should map to their handlers.
         $this->app->make(Dispatcher::class)->mapUsing(function ($command) {
-            return get_class($command).'Handler@handle';
+            return get_class($command) . 'Handler@handle';
         });
+
+        $this->registerDevProviders();
+    }
+
+    /**
+     * Will register the providers for the local environment.
+     */
+    protected function registerDevProviders()
+    {
+        if ($this->app->environment('local')) {
+            $this->app->register(IdeHelperServiceProvider::class);
+        }
     }
 
     /**
