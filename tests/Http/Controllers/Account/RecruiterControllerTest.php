973fe02978ccb6a22f682d5592e531ed5c56e9e8
Israel Ortuño <ai.ortuno@gmail.com>
2015-11-23 18:37:58 +0100

    Added recruiter testing

---
+++ tests/Http/Controllers/Account/RecruiterControllerTest.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laraveles\Recruiter;
+use Laraveles\User;
+
+class RecruiterControllerTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function test_I_can_set_my_recruiter_info()
+    {
+        $user = factory(User::class)->create();
+
+        $this->actingAs($user)
+             ->visit(route('account.recruiter'))
+             ->type('laraveles', 'company')
+             ->type('laraveles.com', 'website')
+            ->press('Guardar')
+            ->seeInDatabase('recruiters', [
+                'company' => 'laraveles',
+                'website' => 'laraveles.com'
+            ]);
+    }
+
+    public function test_I_can_update_my_recruiter_info()
+    {
+        $user = factory(User::class)->create();
+        $user->recruiter()->save(new Recruiter(['company' => 'laraveles', 'website' => 'laraveles.com']));
+
+        $this->actingAs($user)
+             ->visit(route('account.recruiter'))
+             ->type('foo', 'company')
+             ->type('bar.com', 'website')
+             ->press('Guardar')
+             ->seeInDatabase('recruiters', [
+                 'company' => 'foo',
+                 'website' => 'bar.com'
+             ]);
+    }
+    
+    public function test_I_cant_access_if_not_logged_in()
+    {
+        $this->visit(route('account.recruiter'))
+             ->seePageIs(route('auth.login'));
+    }
+}
